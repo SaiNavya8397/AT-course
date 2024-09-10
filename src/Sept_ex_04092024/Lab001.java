c727feea8c235366b3511ed0c5f3b3397cf40023
LENOVO <LENOVO@DESKTOP-QLL2J95>
2024-09-10 17:27:43 +0530

    This is my first commit

---
+++ src/Sept_ex_04092024/Lab001.java
@@ -0,0 +1,10 @@
+package Sept_ex_04092024;
+
+public class Lab001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Sept_ex_04092024/Lab002.java
@@ -0,0 +1,10 @@
+package Sept_ex_04092024;
+
+public class Lab002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
